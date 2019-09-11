cf16832d57958e413634c125d5e94d25665ee88f
Zsombor Pirok <zsombor.pirok@gmail.com>
2019-09-11 15:44:15 +1200

    Added Gitignore and added project (mobile starter content is ignored)

---
+++ Source/DogGame/DogGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DogGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DOGGAME_API ADogGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
